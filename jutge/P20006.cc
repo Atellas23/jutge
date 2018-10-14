9d4c277d52d782b0640c1f31791fccc906adfbcf
Atellas23 <alex.batlle01@gmail.com>
2018-10-14 12:07:55 +0200

    update

---
+++ jutge/P20006.cc
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int expr(char c,char d) {
+	cin >> c >> d;
+	if (c == '+' or c == '*' or c == '-') {
+		
+	}
+	else if (c == '0' or c == '1' or c == '2' or c == '3' or c == '4' or c == '5' or c == '6' or c == '7' or c == '8' or c == '9') {
+		return c;
+	}
+}
+
+int main() {
+	
+}
