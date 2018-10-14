8ddb4b222e0d0cbbd4027d775c7016e47ef8c381
Atellas23 <alex.batlle01@gmail.com>
2018-10-14 16:26:36 +0200

    update

--- jutge/P20006.cc
+++ jutge/P20006.cc
@@ -1,16 +1,15 @@
 #include <iostream>
 using namespace std;
 
-int expr(char c,char d) {
-	cin >> c >> d;
-	if (c == '+' or c == '*' or c == '-') {
-		
-	}
-	else if (c == '0' or c == '1' or c == '2' or c == '3' or c == '4' or c == '5' or c == '6' or c == '7' or c == '8' or c == '9') {
-		return c;
-	}
+int expr(char c) {
+	cin >> c;
+	if (c == '0' or c == '1' or c == '2' or c == '3' or c == '4' or c == '5' or c == '6' or c == '7' or c == '8' or c == '9') return c-'0';
+	if (c == '+') return expr('a')+expr('b');
+	else if (c == '*') return expr('a')*expr('b');
+	else if (c == '-') return expr('a')-expr('b');
 }
 
 int main() {
-	
+	char c;
+	cout << expr(c) << endl;
 }
