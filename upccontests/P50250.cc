4be4a5e9b7401ae1be6287dde2dc503e45ac1b35
Atellas23 <alex.batlle01@gmail.com>
2019-02-11 17:50:33 +0100

    update

---
+++ upccontests/P50250.cc
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+using vi = vector<int>;
+
+struct coord{
+    int x;
+    int y;
+};
+
+using vector<coord> = vc;
+
+
+int main() {
+    int n,m,t;
+    while (cin >> n >> m >> t) {
+        
+    }
+}
